788b146b1421e05590b09d1f4842d591fa767ab5
slumfrog <kishan.anil@gmail.com>
2019-07-15 14:26:46 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Anil"
+    end
+
+    get '/hometown' do
+        "My hometown is Gorleston"
+    end
+
+    get '/favorite-song' do
+       "My favorite song is River Man"
+    end
+
 end
